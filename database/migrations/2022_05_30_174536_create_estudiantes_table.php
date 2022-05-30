8178b5706da03ca8edc9cf7ff50f7af05d1dc613
itahigarciaglez <itahi.garcia5822@alumnos.udg.mx>
2022-05-30 13:27:56 -0500

    creacion de migraciones

---
+++ database/migrations/2022_05_30_174536_create_estudiantes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Unique;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estudiantes', function (Blueprint $table) {
+            $table->string('nombre')->nullable(false);
+            $table->integer('codigo')->nullable(false)->unique();
+            $table->string('carrera')->nullable(true);
+            $table->integer('creditos');
+            $table->string('correo')->unique()->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estudiantes');
+    }
+};
---
+++ database/migrations/2022_05_30_174547_create_materias_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materias', function (Blueprint $table) {
+            $table->id();
+            $table->integer('creditos')->nullable(false);
+            $table->string('nombre')->nullable(false);
+            $table->string('profesor')->nullable(false);
+            $table->string('turno')->nullable(false);
+            $table->boolean('disponible')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materias');
+    }
+};
